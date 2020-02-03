05f211d3b980bf5f7fc1120610bdfb5ef7445dea
David Obodo <obododavid5@gmail.com>
2020-02-03 21:21:32 +0100

    start interaction

---
+++ main.js
@@ -0,0 +1,3 @@
+var input = document.querySelector('.shortly__form__input input');
+var button = document.querySelector('.shortly__form__input button');
+console.log(input, button);
---
+++ main.ts
@@ -0,0 +1,4 @@
+
+const input = document.querySelector('.shortly__form__input input');
+const button = document.querySelector('.shortly__form__input button');
+console.log(input, button)
