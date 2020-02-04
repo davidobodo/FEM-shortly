d7b8c9024db103b85d254870aeae7ff4a46b2c51
David Obodo <obododavid5@gmail.com>
2020-02-04 12:48:20 +0100

    creating short links successfully

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 var input = document.querySelector('.shortly__form__input input');
 var button = document.querySelector('.shortly__form__input button');
-console.log(input, button);
+var form = document.forms["rel"];
+var handleCreateShortLink = function (data) {
+    var id = data.hashid;
+    var shortLink = "https://rel.ink/" + id;
+    console.log(shortLink);
+};
+var submitLink = function (e) {
+    e.preventDefault();
+    console.log(form.link.value);
+    var data = form.link.value;
+    var headers = new Headers({
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    });
+    var request = new Request(form.action, {
+        method: form.method,
+        headers: headers,
+        body: JSON.stringify({ url: data })
+    });
+    fetch(request)
+        .then(function (res) { return res.json(); })
+        .then(function (data) { return handleCreateShortLink(data); })["catch"](function (err) { return console.log(err); });
+};
+button.addEventListener('click', submitLink);
--- main.ts
+++ main.ts
@@ -1,4 +1,32 @@
 
 const input = document.querySelector('.shortly__form__input input');
 const button = document.querySelector('.shortly__form__input button');
-console.log(input, button)
+const form = document.forms["rel"]
+
+
+const handleCreateShortLink = (data: any) => {
+    const { hashid: id } = data;
+    const shortLink = `https://rel.ink/${id}`
+    console.log(shortLink)
+}
+
+const submitLink = (e) => {
+    e.preventDefault();
+    console.log(form.link.value)
+    const data = form.link.value;
+    const headers = new Headers({
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    });
+    const request = new Request(form.action, {
+        method: form.method,
+        headers: headers,
+        body: JSON.stringify({ url: data })
+    });
+    fetch(request)
+        .then(res => res.json())
+        .then(data => handleCreateShortLink(data))
+        .catch(err => console.log(err))
+}
+
+button.addEventListener('click', submitLink)
